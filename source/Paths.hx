88ab81e9c974b5cdd68eed1565f0c5ce1376587c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-08 23:33:14 -0500

    directories

--- source/Paths.hx
+++ source/Paths.hx
@@ -2,7 +2,7 @@ class Paths
 {
 	public static function asset(path:String):String return 'assets/$path';
 
-	public static function audio(path:String):String return asset('$path.wav');
+	public static function audio(path:String):String return asset('snd/$path.wav');
 
-	public static function texture(path:String):String return asset('$path.png');
+	public static function texture(path:String):String return asset('spr/$path.png');
 }
